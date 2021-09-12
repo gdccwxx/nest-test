2accfbe0b66cb78ff03136a411596d48b98571b4
gdccwxx <gdccwxx@gmail.com>
2021-09-12 12:31:59 +0800

    feat: add logger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: new Logger(),
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
--- src/students/students.service.ts
+++ src/students/students.service.ts
@@ -1,12 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class StudentsService {
+    private readonly logger = new Logger(StudentsService.name);
+
     ImStudent(name?: string) {
+        this.logger.log(`student name is ${name}`);
         return 'Im student ' + name;
     }
 
     getStudentName(id: number) {
+        this.logger.log(`get student id is ${id}`);
         const ID_NAME_MAP = {
             1: 'gdccwxx',
         };
